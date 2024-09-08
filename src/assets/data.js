d80a94d8d02debb456dbad5740545a57e0aa99ec
Antonio Jaramillo Fanta <antoniojaramillofanta@gmail.com>
2024-09-08 13:39:58 +0200

    staff

--- src/assets/data.js
+++ src/assets/data.js
@@ -30,3 +30,100 @@ export const lotteries = [
     disabled: true
   }
 ];
+
+export const prizes =  [
+  {
+    name: "El Gordo",
+    prize: 400000,
+    times: 1
+  },
+  {
+    name: "Segundo Premio",
+    prize: 125000,
+    times: 1
+  },
+  {
+    name: "Tercer Premio",
+    prize: 50000,
+    times: 1
+  },
+  {
+    name: "Cuarto Premio",
+    prize: 20000,
+    times: 2
+  },
+  {
+    name: "Quinto Premio",
+    prize: 6000,
+    times: 8
+  },
+  {
+    name: "La Pedrea",
+    prize: 1000,
+    times: 1794
+  },
+  {
+    name: "Reintegro",
+    prize: 20,
+    times: 9999
+  }
+];
+
+export const proximityRule = (prizeNumber, ticketNumber) => {prizeNumber === ticketNumber + 1 || prizeNumber === ticketNumber - 1 }
+export const threeFirstDigitsRule = (prizeNumber, ticketNumber) => {prizeNumber === ticketNumber.slice(0, 3)}
+export const twoLastDigitsRule = (prizeNumber, ticketNumber) => {prizeNumber === ticketNumber.slice(-2)}
+
+export const proximities = [
+  { name: 'Aproximación 1º Premio',
+    prize: 2000,
+    rule: proximityRule,
+  },
+  {
+    name: 'Aproximación 2º Premio',
+    prize: 1000,
+    rule: proximityRule,
+  },
+  {
+    name: 'Aproximación 3º Premio',
+    prize: 1000,
+    rule: proximityRule,
+  }
+]
+
+export const threeFirstDigitPrizes = [
+  {
+    name: 'Centenas 1º Premio',
+    prize: 100,
+    rule: threeFirstDigitsRule,
+  },
+  {
+    name: 'Centenas 2º Premio',
+    prize: 100,
+    rule: threeFirstDigitsRule,
+  },
+  {
+    name: 'Centenas 3º Premio',
+    prize: 100,
+    rule: threeFirstDigitsRule,
+  },
+  {
+    name: 'Centenas 4º Premio',
+    prize: 100,
+    rule: threeFirstDigitsRule,
+  }
+]
+
+export const lastTwoDigitsPrizes = [
+  {
+    name: '2 últimas cifras 1º Premio',
+    prize: 100,
+  },
+  {
+    name: '2 últimas cifras 2º Premio',
+    prize: 100,
+  },
+  {
+    name: '2 últimas cifras 3º Premio',
+    prize: 100,
+  }
+]
